622fb01dc7d2c958ae40d382a4155cc8cd81ed9a
David Velazquez <davidvelazquez@Davids-MacBook-Pro.local>
2023-01-28 16:23:36 -0600

    Add input state

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const DOMContent = document.querySelector(".content");
 const inputInitialPlaceholder = "Type / for blocks, @ to link docs or people";
-const headingPlaceholder = "Heading 1"
-const normalTextPlaceholder = "Normal text"
+const headingPlaceholder = "Heading 1";
+const normalTextPlaceholder = "Normal text";
 const headingPlaceholderStylesConfig = [
   { height: "30px", fontSize: "30px", placeHolder: headingPlaceholder }
 ];
 const normalTextPlaceholderStylesConfig = [
   { height: "20px", fontSize: "12px", placeHolder: normalTextPlaceholder }
 ];
+
+let inputBuildElementState = false
 
 const setInputConfig = (el, value, { height, fontSize, placeHolder }) => {
   el.style.height = height;
@@ -30,47 +32,58 @@ const getHeadingElement = (value) => {
 };
 
 const getNormalTextElement = (value) => {
-    const normalTextElement = document.createElement('p')
-    normalTextElement.appendChild(document.createTextNode(value))
-    return normalTextElement
-}
+  const normalTextElement = document.createElement("p");
+  normalTextElement.appendChild(document.createTextNode(value));
+  return normalTextElement;
+};
+
+const inputKeyUpEvent = (inputElement, event) => {
+  const {
+    code,
+    target: { value }
+  } = event;
+  const DOMContentHasHeading = DOMContent.children.length === 1;
+
+  //Sets input configurations
+  if (RegExp("^/1").test(value)) {
+    inputBuildElementState = true
+    if (DOMContentHasHeading) {
+      headingPlaceholderStylesConfig.forEach((itemConfig) => {
+        setInputConfig(inputElement, value, itemConfig);
+      });
+    } else {
+      normalTextPlaceholderStylesConfig.forEach((itemConfig) => {
+        setInputConfig(inputElement, value, itemConfig);
+      });
+    }
+  }
+
+  //Resets input config
+  if (!value && code === "Backspace") {
+    inputBuildElementState = false
+    resetInputConfig(inputElement);
+  }
+
+  //Creates text elements when hitting enter/return
+  if (code === "Enter" && value.length > 0 && inputBuildElementState) {
+    if (DOMContentHasHeading) {
+      DOMContent.insertBefore(getHeadingElement(value), inputElement);
+      resetInputConfig(inputElement);
+    } else {
+      DOMContent.insertBefore(getNormalTextElement(value), inputElement);
+      resetInputConfig(inputElement);
+    }
+    inputBuildElementState = false
+  }
+};
 
 function inputComponent() {
   const inputElement = document.createElement("input");
 
   inputElement.setAttribute("placeholder", inputInitialPlaceholder);
 
-  inputElement.addEventListener("keyup", ({ code, target: { value } }) => {
-    const DOMContentOnlyChild = DOMContent.children.length === 1;
-
-    //Sets input styles for heading
-    if (RegExp("^/1").test(value)) {
-      if (DOMContentOnlyChild) {
-        headingPlaceholderStylesConfig.forEach((itemConfig) => {
-          setInputConfig(inputElement, value, itemConfig);
-        });
-      } else {
-        normalTextPlaceholderStylesConfig.forEach((itemConfig) => {
-          setInputConfig(inputElement, value, itemConfig);
-        });
-      }
-    }
-
-    //Resets input styles
-    if (code === "Backspace" && value === "" && inputElement.getAttribute('placeholder') === (headingPlaceholder || normalTextPlaceholder) ) {
-      resetInputConfig(inputElement);
-    }
-
-    //Creates text elements when hitting enter/return
-    if (code === "Enter" && value.length > 0) {
-        if(DOMContentOnlyChild) {
-            DOMContent.insertBefore(getHeadingElement(value), inputElement);
-            resetInputConfig(inputElement);
-        } else {
-            DOMContent.insertBefore(getNormalTextElement(value), inputElement);
-            resetInputConfig(inputElement);
-        }
-    }
+  inputElement.addEventListener("keyup", (e) => {
+    inputKeyUpEvent(inputElement, e);
   });
 
   return inputElement;
